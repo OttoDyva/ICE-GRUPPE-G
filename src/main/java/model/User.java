513238fb6f62b22fb59bfee9ba51938b476196b7
OttoDyva <cph-ot@cphbusiness.dk>
2023-12-08 11:34:23 +0100

    BingBong

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,20 @@
 package model;
 
 public class User {
+
+    private String username;
+    private String password;
+    private double height;
+    private double weight;
+    private int age;
+    private String gender;
+
+    public User(String username, String password, double height, double weight, int age, String gender) {
+        this.username = username;
+        this.password = password;
+        this.height = height;
+        this.weight = weight;
+        this.age = age;
+        this.gender = gender;
+    }
 }
