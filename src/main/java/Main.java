be61af69e6d7f2da4b425eccad0d57a46953926d
BekhanM <therealbekhan@gmail.com>
2023-12-08 11:21:58 +0100

    get rich

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null,"Hello, motherfucker");
+        JOptionPane.showMessageDialog(null,"Get rich or die tryin");
+
     }
 }
