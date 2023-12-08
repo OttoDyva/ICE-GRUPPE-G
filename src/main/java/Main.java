6f7374899d29bca8ac4d15f35408ff49c6f566bc
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2023-12-08 11:13:43 +0100

    hej smukke

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null,"Hello, sfasfasf");
+        JOptionPane.showMessageDialog(null,"Hello, Otto :' )");
     }
 }
--- src/main/java/util/TextUI.java
+++ src/main/java/util/TextUI.java
@@ -1,4 +1,7 @@
 package util;
 
 public class TextUI {
+    public void methodeaw(){
+        System.out.println("i like to eat ass");
+    }
 }
