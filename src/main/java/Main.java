f313639e5eceaafb5fd0af46af510b0adcb1d69d
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2023-12-08 11:22:38 +0100

    rap bars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[]args){
-        System.out.println("gucci glip glops suck a nigga dick through the sock");
+        System.out.println("gucci flip flops suck a nigga dick through the sock");
     }
 }
