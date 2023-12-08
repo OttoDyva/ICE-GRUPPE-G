9ce2941fdc5c77757583bacbe508931980b8ea59
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2023-12-08 11:21:46 +0100

    rap bars

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[]args){
-        System.out.println("GitHub sut crackcocaine");
+        System.out.println("gucci glip glops suck a nigga dick through the sock");
     }
 }
